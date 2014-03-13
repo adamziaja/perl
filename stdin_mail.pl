724f3188d0cf52d71ddeb6a8794d58445387a2e6
Adam Ziaja <adamziaja@users.noreply.github.com>
2014-03-13 12:29:57 +0100

    STDIN e-mail

---
+++ stdin_mail.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+# vim:ft=perl
+
+# STDIN e-mail
+# (C) 2014 Adam Ziaja <adam@adamziaja.com> http://adamziaja.com
+
+use strict;
+use warnings;
+use diagnostics;
+
+use Net::SMTP;
+use Email::Date::Format qw(email_date);
+
+my $from    = 'from@example.com';
+my $to      = 'to@example.com';
+my $subject = 'subject';
+my $admin   = 'admin@example.com';
+my $mailsrv = '127.0.0.1';
+
+my $content = undef;
+while ( defined( my $line = <> ) ) {
+    $content .= $line;
+}
+
+if ( length($content) ) {
+    my $smtp = Net::SMTP->new( $mailsrv, Debug => 4 );
+    $smtp->mail($from);
+    $smtp->to($to);
+    $smtp->data();
+    $smtp->datasend("From: Chuck Norris <$from>\n");
+    $smtp->datasend("To: <$to>\n");
+    my $maildate = email_date(time);
+    $smtp->datasend("Date: $maildate\n");
+    $smtp->datasend("Content-Type: text/plain\n");
+    $smtp->datasend("Subject: $subject\n");
+    $smtp->datasend("X-Priority: 1\n");
+    #$smtp->datasend("X-OTRS-Priority: 4\n"); # X-OTRS-Priority: (1 very low|2 low|3 normal|4 high|5 very high)
+    $smtp->datasend("Reply-To: $admin\n");
+    $smtp->datasend("\n");
+    $smtp->datasend($content);
+    $smtp->dataend();
+    $smtp->quit;
+}
