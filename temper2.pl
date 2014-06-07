6c360e5841d927b506a8d1dd6d71c76a41ae93b9
Adam Ziaja <adamziaja@users.noreply.github.com>
2014-06-07 13:17:49 +0200

    TEMPer2

---
+++ temper2.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+# vim:ft=perl
+ 
+# TEMPer2
+# (C) 2014 Adam Ziaja <adam@adamziaja.com> http://adamziaja.com
+ 
+use strict;
+use warnings;
+
+use Device::USB::PCSensor::HidTEMPer;
+
+my $pcsensor = Device::USB::PCSensor::HidTEMPer->new();
+my @devices  = $pcsensor->list_devices();
+  
+foreach my $device ( @devices )
+{
+  print 'internal ' . $device->internal()->celsius() . " \n";
+  print 'external ' . $device->external()->celsius() . "\n";
+}
